3d120ba412fcdc3f3b8d956d8bacac4c551f5168
Meii Lyou (Selina) <33171940+MeiiL@users.noreply.github.com>
2018-01-20 01:39:39 +0800

    Update DHT-kafka-consumer.py

--- 05_Send_IOT_Records/DHT-kafka-consumer.py
+++ 05_Send_IOT_Records/DHT-kafka-consumer.py
@@ -1,70 +1,38 @@
 #!/usr/bin/env python
 
 # -*- coding: utf-8 -*-
-
 from kafka import KafkaConsumer
-
 from kafka.errors import KafkaError
-
 import json
-
 import time
 
 
 class Kafka_consumer():
-
-
     def __init__(self, kafkahost, kafkaport, kafkatopic):
-
         self.kafkaHost = kafkahost
-
         self.kafkaPort = kafkaport
-
         self.kafkatopic = kafkatopic
-
         #self.groupid = groupid
-
         self.consumer = KafkaConsumer(self.kafkatopic,
-
                                       bootstrap_servers='{kafka_host}:{kafka_port}'.format(
-
                                           kafka_host=self.kafkaHost,
-
                                           kafka_port=self.kafkaPort))
 
-
-
     def consume_data(self):
-
         try:
-
             for message in self.consumer:
-
                 # print json.loads(message.value)
-
                 yield message
-
         except KeyboardInterrupt as e:
-
             print(e)
 
-
-
-
-
 def main():
-
-
     consumer = Kafka_consumer("127.0.0.1",9092,"DHTtest")
-
     message = consumer.consume_data()
     for i in message:
         print(i.value)
 
-
-
 if __name__ == '__main__':
-
     main()
 
 
