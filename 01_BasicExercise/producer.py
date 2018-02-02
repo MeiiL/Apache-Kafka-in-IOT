16b35cb1a047d50abd17e8c0da9cadf7c52cfe13
Meii Lyou (Selina) <33171940+MeiiL@users.noreply.github.com>
2018-02-02 13:21:38 +0800

    Update producer.py

--- 01_BasicExercise/producer.py
+++ 01_BasicExercise/producer.py
@@ -6,6 +6,6 @@ while True:
     ts = int(time.time() * 1000)
     producer.send(topic="test", value=str(i), key=str(i), timestamp_ms=ts)
     producer.flush()
-    print i
+    print (i)
     i += 1
     time.sleep(1)
